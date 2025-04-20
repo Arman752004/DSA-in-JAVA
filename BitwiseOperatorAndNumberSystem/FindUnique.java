b0ef33a990dd1f33be7538e7d60fbc7545a903b8
Arman752004 <armanpanda2000@gmail.com>
2025-04-20 19:21:04 +0530

    Your commit message

--- BitwiseOperatorAndNumberSystem/FindUnique.java
+++ BitwiseOperatorAndNumberSystem/FindUnique.java
@@ -2,6 +2,12 @@ package BitwiseOperatorAndNumberSystem;
 
 public class FindUnique {
     public static void main(String[] args) {
+        int[] arr = {2,3,3,4,2,6,4};
+        System.out.println(ans(arr));
+    }
+
+    private static int ans(int){
+        int unique = 0;
         
     }
 }
