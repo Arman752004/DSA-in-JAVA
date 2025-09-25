16174c0cf437a41f3d4a2d1fe0ec4b05eda51457
Arman752004 <armanpanda2000@gmail.com>
2025-09-25 15:31:04 +0530

    Adding StackAndQueue File

---
+++ StackAndQueue/DynamicQueue.java
@@ -0,0 +1,31 @@
+package StackAndQueue;
+
+public class DynamicQueue extends CircularQueue{
+    public DynamicQueue(){
+        super();
+    }
+    public DynamicQueue(int size){
+        super(size);
+    }
+
+    @Override
+    public boolean insert(int item){
+        //this takes care of being full
+        if(this.isFull()){
+            //double the array size
+            int[] temp = new int[data.length * 2];
+
+            //copy all previous items in the new data
+            for (int i = 0; i < data.length; i++) {
+                temp[i] = data[(front + i) % data.length];
+            }
+            front = 0;
+            end = data.length;
+            data = temp;
+        }
+
+        //at this point we dont know array is full
+        //insert item
+        return super.insert(item);
+    }
+}
